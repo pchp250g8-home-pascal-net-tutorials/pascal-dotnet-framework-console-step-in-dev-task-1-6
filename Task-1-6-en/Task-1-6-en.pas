b2a6377ab2d4af67c3b30723268812e69deb580f
Alexander <pchp250g8@tutamail.com>
2026-09-11 18:07:42 +0300

    Created repositary Task-1-6

---
+++ Task-1-6-en/Task-1-6-en.pas
@@ -0,0 +1,20 @@
+﻿uses
+  System;
+begin
+  var n : Cardinal; // Fence length
+  var k : Cardinal; // Paint consumption rate
+  var c : Cardinal; // Cost of one can of paint
+  Console.Write('Enter fence length (in meters): ');
+  Cardinal.TryParse(Console.ReadLine(), n);
+  Console.Write('How many meters does a can of paint last for? ');
+  Cardinal.TryParse(Console.ReadLine(), k);
+  Console.Write('Price of one can of paint (in rubles): ');
+  Cardinal.TryParse(Console.ReadLine(), c);
+  // Number Of cans Of paint For the entire length Of the fence
+  var b : Cardinal := n div k + n mod k;
+  var p : Cardinal := b * c; // Cost Of all cans Of paint For the entire fence
+  // -- Display information on the screen --
+  Console.WriteLine($'{b} cans of paint are needed for {n} meters of fence.');
+  Console.WriteLine($'{p} rubles are needed for {b} cans of paint.');
+  Console.Read(); // Press key "Enter" to close window
+end.
---
+++ Task-1-6-ru/Task-1-6-ru.pas
@@ -0,0 +1,19 @@
+﻿uses
+  System;
+begin
+  var n : Cardinal; // Длина забора 
+  var k : Cardinal; // Величина длины расхода банки краски
+  var c : Cardinal; // Цена одной банки краски
+  Console.Write('Введите длину забора (в метрах): ');
+  Cardinal.TryParse(Console.ReadLine(), n);
+  Console.Write('На сколько метров хватает банки краски? ');
+  Cardinal.TryParse(Console.ReadLine(), k);
+  Console.Write('Цена одной банки краски (в рублях): ');
+  Cardinal.TryParse(Console.ReadLine(), c);
+  var b : Cardinal := n div k + n mod k; // Количество банок краски на всю длину забора
+  var p : Cardinal := b * c; // Стоимость всех банок краски для всего забора
+  // -- Вывод информации на экран --
+  Console.WriteLine($'На {n} метров забора нужно {b} банок(и) краски.');
+  Console.WriteLine($'На {b} банок(и) краски надо потратить {p} рублей.');
+  Console.Read(); // Нажать клавишу ввод для закрытия окна
+end.
